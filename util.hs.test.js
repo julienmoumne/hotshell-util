ccbef67d702ed5bf1e54b2a7d6aaa7c468b5aa8c
Julien Moumné <julien@moumne.com>
2016-11-01 20:51:26 +0100

    test: testing travis-ci integration with a failed case

--- util.hs.test.js
+++ util.hs.test.js
@@ -14,7 +14,7 @@ describe('util', function () {
                 {
                     key: 't',
                     desc: 'desc',
-                    cmd: 'read -r -p "Are you sure? [y/n] " resp; if [[ $resp = y ]]; then cmd; fi'
+                    cmd: 'BUG read -r -p "Are you sure? [y/n] " resp; if [[ $resp = y ]]; then cmd; fi'
                 },
                 util.result
             );
